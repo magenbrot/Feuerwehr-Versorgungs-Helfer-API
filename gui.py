35a1e11163dba5ed017d2802ed60372d2f919f8b
Oliver Völker <oliver@voelker.me>
2025-10-26 11:07:23 +0100

    fixes api user management

--- gui.py
+++ gui.py
@@ -2447,7 +2447,7 @@ def admin_api_user_detail(admin_user, api_user_id_route):
 
 @app.route('/admin/api_user/<int:api_user_id_route>/generate_key', methods=['POST'])
 @admin_required
-def admin_generate_api_key_for_user(api_user_id_route):
+def admin_generate_api_key_for_user(_admin_user, api_user_id_route):
     """
     Generiert einen neuen API-Key für einen spezifischen API-Benutzer.
 
@@ -2486,7 +2486,7 @@ def admin_generate_api_key_for_user(api_user_id_route):
 
 @app.route('/admin/api_key/<int:api_key_id_route>/delete', methods=['POST'])
 @admin_required
-def admin_delete_api_key(api_key_id_route):
+def admin_delete_api_key(_admin_user, api_key_id_route):
     """
     Löscht einen spezifischen API-Key.
 
@@ -2527,7 +2527,7 @@ def admin_delete_api_key(api_key_id_route):
 
 @app.route('/admin/api_user/<int:api_user_id_route>/delete', methods=['POST'])
 @admin_required
-def admin_delete_api_user(api_user_id_route):
+def admin_delete_api_user(_admin_user, api_user_id_route):
     """
     Löscht einen API-Benutzer und alle zugehörigen API-Keys.
 
